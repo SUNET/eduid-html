ec869efc92e02a36565a3777cb0aa214ee993a37
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-07-13 19:48:03 +0200

    webpack config with babel transpiler

---
+++ react/webpack.config.js
@@ -0,0 +1,38 @@
+
+var path = require('path');
+
+module.exports = {
+    entry: {
+      personal_data: './src/personal-data'
+    },
+    output: {
+      path: path.join(__dirname, '/../static/js'),
+      filename: '[name]-bundle.js'
+    },
+    devtool: 'source-map',
+    resolve: {
+      // allow us to import components in tests like:
+      // import Example from 'components/Example';
+      root: [
+        path.join(__dirname, 'src')
+      ],
+      // allow us to avoid including extension name
+      extensions: ['', '.js', '.jsx'],
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015', 'react']
+          }
+        },
+        {
+          test: /\.json$/,
+          loader: 'json'
+        }
+      ]
+    }
+};
