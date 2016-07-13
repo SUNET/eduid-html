9372347830ee115b71c245afb96b745b1806131d
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-07-13 20:00:43 +0200

    production webpack entry point for personal data widget

---
+++ react/src/personal-data.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { PersonalData } from 'components/PersonalData';
+
+ReactDOM.render(<PersonalData />, document.getElementById('root'));
