d802c117dbeccce6ff04d24e9f5f381abe189c39
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-07-15 14:30:35 +0200

    an example for testing purposes

--- react/src/components/TextEntry.js
+++ react/src/components/TextEntry.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import {FormattedMessage, FormattedNumber, FormattedRelative} from 'react-intl';
+
 
 export default class TextEntry extends Component {
   render () {
     return (
         <div className="text-entry">
+          <span>
+            <FormattedMessage
+                    id="greeting.welcome_message"
+                    defaultMessage={`
+                        Welcome {name}, you have received {unreadCount, plural,
+                            =0 {no new messages}
+                            one {{formattedUnreadCount} new message}
+                            other {{formattedUnreadCount} new messages}
+                        }.
+                    `}
+                    values={{
+                        name: <b>uno</b>,
+                        unreadCount: 2,
+                        formattedUnreadCount: (
+                            <b><FormattedNumber value={2} /></b>
+                        ),
+                    }}
+            />
+          </span>
           <input type="text" name={this.props.name} />
         </div>
     );
