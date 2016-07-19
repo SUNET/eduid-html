d5da51df22acf88138bfcc1074fdd4c70e1acea5
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-07-19 15:15:35 +0200

    localize according to browser language

--- react/src/components/PersonalData.js
+++ react/src/components/PersonalData.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import { IntlProvider, addLocaleData } from 'react-intl';
-import sv from 'react-intl/locale-data/sv';
-import svMessages from '../../l10n/sv.json';
 
 import TextEntry from 'components/TextEntry';
 import TextSelect from 'components/TextSelect';
 
-addLocaleData(sv);
+const language = navigator.languages
+                   ? navigator.languages[0]
+                   : (navigator.language || navigator.userLanguage);
+
+const lang_code = language.substring(0,2);
+
+// XXX Horrible hack: we can only import from literal strings,
+// not from a string in a var, neither from a string built on the spot.
+// So we have to import all locales :(
+import enLocale from 'react-intl/locale-data/en';
+const enMessages = {};
+import svLocale from 'react-intl/locale-data/sv';
+import svMessages from '../../l10n/sv.json';
+import esLocale from 'react-intl/locale-data/es';
+import esMessages from '../../l10n/es.json';
+
+if (lang_code === 'en') {
+  var locale = enLocale,
+      messages = enMessages;
+} else if (lang_code === 'sv') {
+  var locale = svLocale,
+      messages = svMessages;
+} else if (lang_code === 'es') {
+  var locale = esLocale,
+      messages = esMessages;
+}
+// XXX end horrible hack
+
+addLocaleData(locale);
 
 
 export default class PersonalData extends Component {
   render () {
     return (
-        <IntlProvider locale={ 'sv-SE' } messages={ svMessages }>
+        <IntlProvider locale={ lang_code } messages={ messages }>
           <div id="personal-data-form" className="eduid-form">
             <TextEntry name="given_name" />
             <TextEntry name="surname" />
