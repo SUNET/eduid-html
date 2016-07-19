2ab8b6a337056888a26df6320db9138a737efd96
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-07-19 13:18:29 +0200

    webpack config for production builds

---
+++ react/webpack.prod.config.js
@@ -0,0 +1,27 @@
+
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config');
+
+var webpackProd = {
+  entry: webpackConfig.entry,
+  output: webpackConfig.output,
+  resolve: webpackConfig.resolve,
+  module: webpackConfig.module
+};
+
+webpackProd.output.filename = '[name]-bundle.prod.js';
+
+webpackProd.plugins = [
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurrenceOrderPlugin(true),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+    },
+      output: {
+      comments: false,
+    },
+  })
+];
+
+module.exports = webpackProd;
