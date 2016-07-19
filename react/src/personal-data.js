205c69b110b80c923f55526f8070b836046ee47f
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-07-19 13:26:47 +0200

    wrong import of default export

--- react/src/personal-data.js
+++ react/src/personal-data.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { PersonalData } from 'components/PersonalData';
+import PersonalData from 'components/PersonalData';
 
 ReactDOM.render(<PersonalData />, document.getElementById('root'));
