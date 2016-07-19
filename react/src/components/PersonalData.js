9187f4c259b0f65c0a10cb13f9a0c6978385b23b
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-07-19 13:23:42 +0200

    localize component to swedish

--- react/src/components/PersonalData.js
+++ react/src/components/PersonalData.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import sv from 'react-intl/locale-data/sv';
+import svMessages from '../../l10n/sv-SE.json';
 
 import TextEntry from 'components/TextEntry';
 import TextSelect from 'components/TextSelect';
+
+addLocaleData(sv);
 
 
 export default class PersonalData extends Component {
   render () {
     return (
-        <div id="personal-data-form" className="eduid-form">
-          <TextEntry name="given_name" />
-          <TextEntry name="surname" />
-          <TextEntry name="display_name" />
-          <TextSelect name="language" />
-        </div>
+        <IntlProvider locale={ 'sv-SE' } messages={ svMessages }>
+          <div id="personal-data-form" className="eduid-form">
+            <TextEntry name="given_name" />
+            <TextEntry name="surname" />
+            <TextEntry name="display_name" />
+            <TextSelect name="language" />
+          </div>
+        </IntlProvider>
     );
   }
 }
