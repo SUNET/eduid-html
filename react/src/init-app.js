6b48f26f712ffef897158f3929baaf2897d97d4d
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-11-29 15:05:37 +0100

    change redux-thunk for redux-saga to deal with asynchronous actions - WIP

--- react/src/init-app.js
+++ react/src/init-app.js
@@ -10,13 +10,19 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom';
-import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware, { take } from 'redux-saga'
+import { call, put } from 'redux-saga/effects'
 import createLogger from 'redux-logger';
 import { Provider } from 'react-redux';
 import { IntlProvider, addLocaleData } from 'react-intl';
 import { createStore, applyMiddleware } from "redux";
 import eduIDApp from "./store";
 import { fetchConfig } from "actions/Config";
+import * as configActions from "actions/Config";
+import * as pdataActions from "actions/PersonalData";
+import { requestPersonalData } from "sagas/PersonalData";
+
+/* i18n */
 
 const language = navigator.languages
                    ? navigator.languages[0]
@@ -28,12 +34,28 @@ const messages = require('../i18n/l10n/' + lang_code)
 
 addLocaleData(locale);
 
+/* Sagas */
+
+function* rootSaga() {
+  yield [
+    take(configActions.GET_JSCONFIG_CONFIG_SUCCESS, requestPersonalData)
+  ];
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+/* Store */
+
 export const store = createStore(
         eduIDApp,
         applyMiddleware(
-            thunkMiddleware,
+            sagaMiddleware,
             createLogger()
             ));
+
+sagaMiddleware.run(rootSaga);
+
+/* render app */
 
 const init_app = function (component, target) {
   let app = ( <Provider store={store}>
---
+++ react/src/sagas/PersonalData.js
@@ -0,0 +1,27 @@
+
+import { checkStatus, ajaxHeaders } from "sagas/common";
+import { getUserData, getUserDataFail } from "actions/PersonalData";
+
+
+export function* requestPersonalData () {
+    try {
+        yield put(getUserData());
+        const config = select(state => state.config);
+        yield call(fetchPersonalData, config);
+    } catch(error) {
+        yield put(getUserDataFail(error.toString()));
+    }
+}
+
+
+function* fetchPersonalData (config) {
+    yield window.fetch(config.PERSONAL_DATA_URL, {
+      // To automatically send cookies only for the current domain,
+      // set credentials to 'same-origin'; use 'include' for CORS
+      credentials: 'include',
+      headers: ajaxHeaders
+    })
+    .then(checkStatus)
+    .then(response => response.json())
+    .then(userdata => put(userdata))
+}
