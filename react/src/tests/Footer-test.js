1e83d9d2f6b67dffbfcae19735b0cc4bef65475e
Enrique Pérez Arnaud <enrique@cazalla.net>
2017-09-18 14:43:39 +0200

    footer tests, WIP

---
+++ react/src/tests/Footer-test.js
@@ -0,0 +1,55 @@
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { shallow, mount, render } from 'enzyme';
+import expect, { createSpy } from "expect";
+import { IntlProvider, addLocaleData } from 'react-intl';
+import { MemoryRouter } from "react-router-dom";
+
+import Footer from 'components/Footer';
+
+const messages = require('../../i18n/l10n/en');
+addLocaleData('react-intl/locale-data/en');
+
+
+const fakeStore = (state) => ({
+    default: () => {},
+    dispatch: createSpy(),
+    subscribe: createSpy(),
+    getState: () => ({ ...state })
+});
+
+function setupComponent() {
+    const store = fakeStore({
+        config: {
+            is_configured: true,
+            language: 'en',
+            AVAILABLE_LANGUAGES: [
+                ['en', 'English'],
+                ['sv', 'Svenska']
+            ]
+        }
+    });
+    const props = { language: 'en' };
+    const wrapper = mount(<Provider store={ store }>
+                              <IntlProvider locale={'en'} messages={messages}>
+                                  <Footer {...props} />
+                              </IntlProvider>
+                          </Provider>);
+    return {
+        props,
+        wrapper
+    };
+}
+
+describe("Footer Component", () => {
+
+    it("Renders", () => {
+        const { wrapper, props } = setupComponent(),
+              container = wrapper.find('div.container'),
+              selector = wrapper.find('span.langselector');
+
+        expect(container.contains(selector.get(0))).toBeTruthy();
+    });
+});
+
