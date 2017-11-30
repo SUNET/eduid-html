727c64901826766d4c26f512e710d7b975e445c9
Johan Lundberg <lundberg@sunet.se>
2017-11-30 17:32:05 +0100

    Clean up docker container
    Use sed instead of generating full index.html
    
    You need to change docker volumes for eduid-html in eduid-developer as
    follows:
         volumes:
           - ../eduid-html/log:/var/log/nginx
    -      - ../eduid-html/etc:/etc/nginx/sites-enabled
           # source volumes
    -      - ../sources/eduid-html:/opt/eduid/eduid-html
    +      - ../sources/eduid-html:/opt/eduid/www/
    +      - ../sources/eduid-html/static:/opt/eduid/static/
    +      - ../sources/eduid-html/react/build:/opt/eduid/static/build/

--- docker/start.sh
+++ docker/start.sh
@@ -4,26 +4,9 @@ set -e
 
 ENTRY_POINT_URL=${ENTRY_POINT_URL-'https://www.eduid.se/static/build/index.js'}
 
-cat>/opt/eduid/eduid-html/wwwroot/profile/index.html<<EOF
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>eduID</title>
-  </head>
-  <body>
-    <noscript>
-      You need to enable JavaScript to run this app.
-    </noscript>
-    <div id="root" class="wrap container"></div>
-    <script type="text/javascript" src="${ENTRY_POINT_URL}"></script>
-  </body>
-</html>
-EOF
+/bin/sed -i -e "s#ENTRY_POINT_URL#${ENTRY_POINT_URL}#g" /opt/eduid/profile/index.html
 
-echo "Created /opt/eduid/eduid-html/wwwroot/profile/index.html with ENTRY_POINT_URL=${ENTRY_POINT_URL}."
+echo "Updated /opt/eduid/profile/index.html with ENTRY_POINT_URL=${ENTRY_POINT_URL}."
 
 chgrp www-data /var/log/nginx
 
