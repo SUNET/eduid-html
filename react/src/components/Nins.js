1931193efb9941c0b4ab33b73d3c10faf1f2ca76
Johan Lundberg <lundberg@sunet.se>
2017-12-14 16:14:29 +0100

    Indentation fix

--- react/src/components/Nins.js
+++ react/src/components/Nins.js
@@ -12,25 +12,25 @@ import 'style/Nins.scss';
 
 
 const validate = values => {
-  let value  = values.norEduPersonNin;
+  let value = values.norEduPersonNin;
   // accept only digits
   if (/[^0-9]+/.test(value)) return {norEduPersonNin: 'nins.illegal_chars'};
-	if (value.length !== 12) return {norEduPersonNin: 'nins.wrong_length'};
+  if (value.length !== 12) return {norEduPersonNin: 'nins.wrong_length'};
 
-	// The Luhn Algorithm. It's so pretty.
+  // The Luhn Algorithm. It's so pretty.
   // taken from https://gist.github.com/DiegoSalazar/4075533/
-	let nCheck = 0, bEven = false;
-	value = value.slice(2);  // To pass the Luhn check only use the 10 last digits
-	for (let n = value.length - 1; n >= 0; n--) {
-		let cDigit = value.charAt(n),
-			  nDigit = parseInt(cDigit, 10);
-		if (bEven) {
-			if ((nDigit *= 2) > 9) nDigit -= 9;
-		}
-		nCheck += nDigit;
-		bEven = !bEven;
-	}
-	if ( (nCheck % 10) !== 0) {
+  let nCheck = 0, bEven = false;
+  value = value.slice(2);  // To pass the Luhn check only use the 10 last digits
+  for (let n = value.length - 1; n >= 0; n--) {
+    let cDigit = value.charAt(n),
+      nDigit = parseInt(cDigit, 10);
+    if (bEven) {
+      if ((nDigit *= 2) > 9) nDigit -= 9;
+    }
+    nCheck += nDigit;
+    bEven = !bEven;
+  }
+  if ((nCheck % 10) !== 0) {
     return {norEduPersonNin: 'nins.invalid_nin'};
   }
   return {}
