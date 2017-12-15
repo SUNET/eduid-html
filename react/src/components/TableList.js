ccdc30c6276c0171139b547268c0119e099784de
Johan Lundberg <lundberg@sunet.se>
2017-12-15 15:30:26 +0100

    Allow removal of primary email and phone

--- react/src/components/TableList.js
+++ react/src/components/TableList.js
@@ -27,9 +27,10 @@ class TableList extends Component {
                         <span className="nobutton">{this.props.l10n('tl.primary')}</span>
                     </td>
                     <td className="non-identifier">
-                    <EduIDButton className="text-muted" bsStyle="link">
-                            {this.props.l10n('tl.remove')}
-                        </EduIDButton>
+                    <EduIDButton bsStyle="link"
+                                 onClick={this.props.handleRemove}>
+                      {this.props.l10n('tl.remove')}
+                      </EduIDButton>
                     </td>
                 </tr>);
           } else if (entry.verified) {
