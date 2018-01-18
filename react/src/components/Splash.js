c7df786f2e25d81e33540abca24ff32bc6ffa30b
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-01-18 13:56:20 +0100

    set spinner in splash screen

--- react/src/components/Splash.js
+++ react/src/components/Splash.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactDom from 'react-dom';
+import {Spinner} from 'spin.js';
 
 import 'style/Splash.scss';
 
 
+const spinnerOpts = {
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    fps: 20, // Frames per second when using setTimeout() as a fallback in IE 9
+};
+
 class Splash extends Component {
 
-  render () {
+    componentDidMount () {
+        const splash = this.refs.eduidSplash;
+        const spinner = new Spinner(spinnerOpts).spin();
+        splash.appendChild(spinner.el);
+    }
 
-    return (
-      <div id="eduid-splash-screen" />
-    );
-  }
+    render () {
+
+
+        return (
+            <div ref="eduidSplash" id="eduid-splash-screen" />
+        );
+    }
 }
 
 export default Splash;
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     output: {
       path: path.join(__dirname, 'build'),
-      publicPath: 'http://html.eduid.docker/static/build/',
+      publicPath: 'http://dashboard.eduid.local.emergya.info:8080/static/build/',
       filename: '[name]-bundle.dev.js'
     },
     devtool: 'source-map',
