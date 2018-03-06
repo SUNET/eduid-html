a6836b218e5447896f19f8732340359f504bcfa4
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-03-06 14:37:28 +0100

    redirect manually after lgout

--- react/src/sagas/Header.js
+++ react/src/sagas/Header.js
@@ -8,29 +8,18 @@ import { postLogoutFail, POST_AUTHN_LOGOUT_SUCCESS } from "actions/Header";
 export function* requestLogout () {
     try {
         const state = yield select(state => state),
-              data = {
-                csrf_token: state.config.csrf_token
-              },
-              authn_url = state.config.TOKEN_SERVICE_URL;
-        const resp = yield call(sendLogout, authn_url, data);
-        yield put(putCsrfToken(resp));
-        if (resp.type === POST_AUTHN_LOGOUT_SUCCESS) {
+              url = state.config.TOKEN_SERVICE_URL;
+        window.fetch(url + 'logout', {
+            method: 'get',
+            credentials: 'same-origin',
+            mode: 'no-cors',
+            redirect: 'manual'
+        })
+        .then(resp => {
             const win = resp.testingWindow || window;
-            win.location = resp.payload.location;
-            delete resp.testingWindow;
-        } else {
-            yield put(resp);
-        }
+            win.location = resp.url;
+        });
     } catch(error) {
         yield* failRequest(error, postLogoutFail);
     }
-}
-
-export function sendLogout (url, data) {
-    return window.fetch(url + 'logout', {
-        ...postRequest,
-        body: JSON.stringify(data)
-    })
-    .then(checkStatus)
-    .then(response => response.json())
 }
