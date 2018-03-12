c473ee6e4ae2d6851992d88a4054ada7a91e03ff
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-03-12 11:25:37 +0100

    fix logout for IE11

--- react/src/sagas/Header.js
+++ react/src/sagas/Header.js
@@ -8,16 +8,20 @@ import { postLogoutFail, POST_AUTHN_LOGOUT_SUCCESS } from "actions/Header";
 export function* requestLogout () {
     try {
         const state = yield select(state => state),
-              url = state.config.TOKEN_SERVICE_URL;
-        window.fetch(url + 'logout', {
-            method: 'get',
-            credentials: 'same-origin',
-            mode: 'no-cors',
-            redirect: 'manual'
-        })
-        .then(resp => {
-            window.location = resp.url
-        });
+              url = state.config.TOKEN_SERVICE_URL + 'logout';
+        if (navigator.userAgent.indexOf("Trident/7") > -1) {
+            window.location = url;
+        } else {
+            window.fetch(url, {
+                method: 'get',
+                credentials: 'same-origin',
+                mode: 'no-cors',
+                redirect: 'manual'
+            })
+            .then(resp => {
+                window.location = resp.url
+            });
+        }
     } catch(error) {
         yield* failRequest(error, postLogoutFail);
     }
