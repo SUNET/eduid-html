2787abf3a8631931f8932f93c9559b0c4fd0a1c3
Enrique Pérez Arnaud <enrique@cazalla.net>
2019-05-17 11:12:26 +0200

    remove react stuff from eduid-html

--- react/translationRunner.js
+++
@@ -1,7 +0,0 @@
-const manageTranslations = require('react-intl-translations-manager').default;
-
-manageTranslations({
-  messagesDirectory: 'i18n/src',
-  translationsDirectory: 'i18n/l10n',
-  languages: ['en', 'sv'], // any language you need 
-});
