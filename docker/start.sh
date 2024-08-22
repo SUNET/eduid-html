342786195b377deebc86aa280faccbb53b692cba
Johan Lundberg <lundberg@sunet.se>
2024-08-22 11:56:48 +0200

    there are no more static html

--- docker/start.sh
+++ docker/start.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# Try to mitigate "Hans folly"
-if [ "$ENVIRONMENT" = "staging" ]; then
-  find /opt/eduid/www/ -name "*.html" -writable -exec sed -i 's/href="https:\/\/dashboard.eduid.se\/"/href="https:\/\/dashboard.dev.eduid.se\/"/g' {} +
-  find /opt/eduid/www/ -name "*.html" -writable -exec sed -i 's/href="https:\/\/signup.eduid.se\/"/href="https:\/\/signup.dev.eduid.se\/"/g' {} +
-fi
-
 chgrp www-data /var/log/nginx
 
 exec start-stop-daemon --start --exec \
