95d1d6f643b5cc20579cbdbe544cb5bc6f68e10c
Eunju Huss <eunju@sunet.se>
2025-07-22 13:11:43 +0200

    Add error messages and stylings using javascript

--- static/js/support-function.js
+++ static/js/support-function.js
@@ -10,3 +10,56 @@ document.addEventListener("htmx:afterSwap", function () {
     });
   }
 });
+
+document.body.addEventListener("htmx:afterSettle", function () {
+  const errorDiv = document.getElementById("errors");
+
+  if (errorDiv && errorDiv.textContent.trim().toLowerCase() === "not authorized" && !errorDiv.querySelector("p")) {
+    const msg = document.createElement("p");
+    msg.innerHTML =
+      "It seems you don't have permission to access the support page.<br>" +
+      "Please click the login button in the header to continue.";
+    msg.style.marginTop = "8px";
+    msg.style.fontSize = "15px";
+    msg.style.fontFamily = "Inter-regular";
+    msg.style.color = "#5e5e5e";
+    errorDiv.appendChild(msg);
+  }
+});
+
+// let draggedRow = null;
+
+// document.querySelectorAll(".row").forEach((row) => {
+//   row.addEventListener("dragstart", (e) => {
+//     draggedRow = row;
+//     e.dataTransfer.effectAllowed = "move";
+//     e.dataTransfer.setData("text/plain", row.id);
+//     row.style.opacity = "0.5";
+//   });
+
+//   row.addEventListener("dragend", () => {
+//     draggedRow.style.opacity = "1";
+//     draggedRow = null;
+//   });
+
+//   row.addEventListener("dragover", (e) => {
+//     e.preventDefault();
+//     e.dataTransfer.dropEffect = "move";
+//   });
+
+//   row.addEventListener("drop", (e) => {
+//     e.preventDefault();
+//     if (draggedRow === row) return;
+
+//     const container = document.querySelector(".panel-body");
+//     console.log("container", container);
+//     const draggedIndex = Array.from(container.children).indexOf(draggedRow);
+//     const targetIndex = Array.from(container.children).indexOf(row);
+
+//     if (draggedIndex < targetIndex) {
+//       container.insertBefore(draggedRow, row.nextSibling);
+//     } else {
+//       container.insertBefore(draggedRow, row);
+//     }
+//   });
+// });
