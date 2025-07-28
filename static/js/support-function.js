a76d2f38261f20ada78742cdbf2b7ca153d70c6a
Eunju Huss <eunju@sunet.se>
2025-07-28 12:22:17 +0200

    Move styling into JavaScript

--- static/js/support-function.js
+++ static/js/support-function.js
@@ -13,16 +13,58 @@ document.addEventListener("htmx:afterSwap", function () {
 
 document.body.addEventListener("htmx:afterSettle", function () {
   const errorDiv = document.getElementById("errors");
-
+  let loginURL;
+  if (location.hostname.includes("dev")) {
+    loginURL = "https://dev.eduid.se/";
+  } else if (location.hostname.includes("docker")) {
+    loginURL = "https://html.eduid.docker/";
+  } else loginURL = "https://eduid.se/";
   if (errorDiv && errorDiv.textContent.trim().toLowerCase() === "not authorized" && !errorDiv.querySelector("p")) {
+    const style = document.createElement("style");
+    style.innerHTML = `
+    div.errors {
+      font-family: "Inter-semibold", Arial, Helvetica, sans-serif;
+      position: relative;
+      color: #161616;
+      border-radius: 4px;
+      text-align: center;
+      line-height: 1.5;
+      margin-top: 17rem;
+      font-size: 1.5rem;
+    }
+    div.errors::before {
+      content: "";
+      position: absolute;
+      top: 9px;
+      left: 50%;
+      transform: translateX(-50%);
+      width: 100px;
+      height: 100px;
+      border: 8px solid #161616;
+      border-radius: 50%;
+      margin-top: -9rem;
+    }
+    div.errors::after {
+      content: "";
+      position: absolute;
+      top: 11px;
+      left: 50%;
+      transform: translate(-50%, 0) rotate(-45deg);
+      width: 9px;
+      height: 105px;
+      background-color: #161616;
+      margin-top: -9rem;
+    }
+  `;
     const msg = document.createElement("p");
     msg.innerHTML =
       "It seems you don't have permission to access the support page.<br>" +
-      "Please click the login button in the header to continue.";
+      `Please <a href="${loginURL}">log in</a> to continue.`;
     msg.style.marginTop = "8px";
     msg.style.fontSize = "15px";
     msg.style.fontFamily = "Inter-regular";
     msg.style.color = "#5e5e5e";
+    errorDiv.appendChild(style);
     errorDiv.appendChild(msg);
   }
 });
